8d86e5d8e44c7b3e8ce9f1e73f0ee64c83682e5f
Gabriel Zonatelle Borges <gabrielzonatelle@gmail.com>
2019-12-06 16:34:53 -0300

    funcao para desenhar paralelgramo; asa desenhada

--- draw.cpp
+++ draw.cpp
@@ -375,3 +375,43 @@ void Draw::drawSphere(Sphere sphere)
 {
     glutSolidSphere(sphere.getRadius(), 100, 100);
 }
+
+void Draw::drawParallelSolid(Point p1, Point p2, Point p3, Point p4, Point p5, Point p6, Point p7, Point p8)
+{
+
+    //top-bottom
+    glBegin(GL_QUADS);
+    glVertex3f(p1.getX(), p1.getY(), p1.getZ());
+    glVertex3f(p2.getX(), p2.getY(), p2.getZ());
+    glVertex3f(p3.getX(), p3.getY(), p3.getZ());
+    glVertex3f(p4.getX(), p4.getY(), p4.getZ());
+    glVertex3f(p5.getX(), p5.getY(), p5.getZ());
+    glVertex3f(p6.getX(), p6.getY(), p6.getZ());
+    glVertex3f(p7.getX(), p7.getY(), p7.getZ());
+    glVertex3f(p8.getX(), p8.getY(), p8.getZ());
+    glEnd();
+
+    //left-right
+    glBegin(GL_QUADS);
+    glVertex3f(p1.getX(), p1.getY(), p1.getZ());
+    glVertex3f(p6.getX(), p6.getY(), p6.getZ());
+    glVertex3f(p7.getX(), p7.getY(), p7.getZ());
+    glVertex3f(p2.getX(), p2.getY(), p2.getZ());
+    glVertex3f(p3.getX(), p3.getY(), p3.getZ());
+    glVertex3f(p8.getX(), p8.getY(), p8.getZ());
+    glVertex3f(p5.getX(), p5.getY(), p5.getZ());
+    glVertex3f(p4.getX(), p4.getY(), p4.getZ());
+    glEnd();
+
+    //back-front
+    glBegin(GL_QUADS);
+    glVertex3f(p4.getX(), p4.getY(), p4.getZ());
+    glVertex3f(p5.getX(), p5.getY(), p5.getZ());
+    glVertex3f(p6.getX(), p6.getY(), p6.getZ());
+    glVertex3f(p1.getX(), p1.getY(), p1.getZ());
+    glVertex3f(p2.getX(), p2.getY(), p2.getZ());
+    glVertex3f(p7.getX(), p7.getY(), p7.getZ());
+    glVertex3f(p8.getX(), p8.getY(), p8.getZ());
+    glVertex3f(p3.getX(), p3.getY(), p3.getZ());
+    glEnd();
+}
